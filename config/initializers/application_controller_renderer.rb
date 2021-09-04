9f57cee75f9846f6f6bac8f9981d4f8cc2e59cc4
EC2 Default User <ec2-user@ip-172-31-0-185.ec2.internal>
2021-09-04 06:32:56 +0000

    [Add]コメント機能/いいね機能/ページング機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd85c7391e3cc2fe31c7fe0545e227eee823f380532b4151095d323ef62a143743dc40c9182947ce3b0147020fabac1fd27eb234ac37a9c594c59f21cf79ed7e4'
