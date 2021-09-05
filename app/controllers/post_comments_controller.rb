0bed49215f39637cb26ae42bf1ebca523ee76d82
EC2 Default User <ec2-user@ip-172-31-0-185.ec2.internal>
2021-09-05 06:07:17 +0000

    [Add]meshiterroにBootstrap適用

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,8 +1,8 @@
 class PostCommentsController < ApplicationController
+
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
-    binding.pry
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
@@ -12,7 +12,6 @@ class PostCommentsController < ApplicationController
     PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
     redirect_to post_image_path(params[:post_image_id])
   end
-  
   private
 
   def post_comment_params
